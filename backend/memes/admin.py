1f551c91cea2d672bc86853121749d3e360ee2d6
mihailkasev <kasevmihail9@gmail.com>
2023-01-29 12:17:16 +0300

    Frontend add

--- backend/memes/admin.py
+++ backend/memes/admin.py
@@ -5,12 +5,12 @@ from .models import Meme, Tag
 @admin.register(Meme)
 class MemeAdmin(admin.ModelAdmin):
     '''Админ-панель модели Meme с фильтрацией по тегам'''
-    list_display = ('id',)
+    list_display = ('id', 'image')
     list_filter = ('tag',)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     '''Админ-панель модели Tag с фильтрацией по названию'''
-    list_display = ('name', 'slug')
+    list_display = ('id', 'name', 'slug')
     list_filter = ('name',)
