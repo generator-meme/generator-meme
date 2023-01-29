9f3de913f8b4c30730664f02fa72acaeeb85e74f
lesjok <lesjok@inbox.ru>
2023-01-29 16:23:52 +0300

    get data from backend

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import Meme from '../Meme/Meme'
 import meme from '../../images/meme-image.png'
-import { memes } from '../../utils/constants'
+// import { memes } from '../../utils/constants'
 import human from '../../images/human.png'
 import lines from '../../images/lines.png'
 import lines2 from '../../images/lines2.png'
@@ -9,7 +9,7 @@ import arrowTop from '../../images/arrow-top.svg'
 import './Main.css'
 import { Link } from 'react-router-dom'
 
-const Main = () => {
+const Main = ({  memes }) => {
   return (
     <div className="main">
       <img className="main__human" src={human} alt="human" />
@@ -29,7 +29,7 @@ const Main = () => {
       </Link>
       <div className="main__memebox">
         {memes.map((elem) => (
-          <Meme image={meme} key={elem} />
+          <Meme image={elem.image} key={elem.id} />
         ))}
       </div>
       <button className="main__btn main__btn_show-more">Показать больше</button>
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,39 +1,38 @@
-// class Api {
-//   constructor(options) {
-//     this._options = options;
-//   }
+class Api {
+  constructor(options) {
+    this._options = options;
+  }
 
-//   _errorHandler(res) {
-//     if (res.ok) {
-//       return res.json();
-//     }
-//     return Promise.reject(`Ошибка: ${res.status}`);
-//   }
+  _errorHandler(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
 
-//   getTemplates() {
-//     return fetch(`${this._options.baseUrl}/api/memes`, {
-//     method: 'GET',
-//     body: JSON.stringify(),
-//     headers: this._options.headers,
-//   })
-//     .then(this._errorHandler)
-//   }
+  getTemplates() {
+    return fetch('/api/memes', {
+    method: 'GET',
+    body: JSON.stringify(),
+    headers: this._options.headers,
+  })
+    .then(this._errorHandler)
+  }
 
-//   getTemplate(id) {
-//     return fetch(`${this._options.baseUrl}/api/memes/${id}`, {
-//     method: 'GET',
-//     headers: this._options.headers,
-//   })
-//     .then(this._errorHandler)
-//   }
+  getTemplate(id) {
+    return fetch(`/api/memes/${id}`, {
+    method: 'GET',
+    headers: this._options.headers,
+  })
+    .then(this._errorHandler)
+  }
 
-// }
+}
 
-// const api = new Api({
-//   baseUrl: 'http://localhost:3000',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
+const api = new Api({
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
-// export default api;
+export default api;
