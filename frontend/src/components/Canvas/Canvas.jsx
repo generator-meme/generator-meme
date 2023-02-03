51eef265fe1a7f6a0c8fb5fae965012e1d9114aa
lesjok <lesjok@inbox.ru>
2023-02-04 00:40:37 +0300

    feat: add some text tools at canvas

--- frontend/src/components/Canvas/Canvas.jsx
+++ frontend/src/components/Canvas/Canvas.jsx
@@ -2,6 +2,12 @@ import back from '../../images/back.svg'
 import React, { useState, useEffect, useRef } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import arrowNav from '../../images/arrow-nav.svg'
+// import photo from '../../images/meme-edit.jpg'
+import textLeft from '../../images/icons/align-left.png'
+import textRight from '../../images/icons/align-right.png'
+import textCenter from '../../images/icons/align-center.png'
+import textBold from '../../images/icons/bold.png'
+import textItalic from '../../images/icons/italic.png'
 import './Canvas.css'
 
 const Canvas = ({ memes }) => {
@@ -15,11 +21,18 @@ const Canvas = ({ memes }) => {
   const [color, setColor] = useState(null)
   const [topFontSize, setTopFontSize] = useState(50)
   const [bottomFontSize, setBottomFontSize] = useState(50)
+  const [bottomFontFamily, setBottomFontFamily] = useState('Comic Sans MS')
+  const [bottomFontPosition, setBottomFontPosition] = useState('center')
+  const [bottomFontWeight, setBottomFontWeight] = useState('normal')
+  const [bottomFontStyle, setBottomFontStyle] = useState('normal')
+  const [topFontFamily, setTopFontFamily] = useState('Comic Sans MS')
+  const [topFontPosition, setTopFontPosition] = useState('center')
+  const [topFontWeight, setTopFontWeight] = useState('normal')
+  const [topFontStyle, setTopFontStyle] = useState('normal')
 
   useEffect(() => {
     const memeImage = new Image()
     memeImage.src = currentMeme.image
-    memeImage.onload = () => setImage(memeImage)
   }, [])
 
   const fillStyle = (color) => {
@@ -30,14 +43,24 @@ const Canvas = ({ memes }) => {
     setFontFunction(size);
   }
 
+  function increaseSize (size, setFontFunction) {
+    setFontFunction(size + 1);
+  }
+
+  function decreaseSize (size, setFontFunction) {
+    setFontFunction(size - 1);
+  }
+
+
   useEffect(() => {
     const ctx = canvas.current.getContext('2d')
     ctx.fillStyle = 'black'
     ctx.drawImage(image, 0, 0)
 
-    ctx.font = `${bottomFontSize}px Comic Sans MS`
+    ctx.font = `${bottomFontStyle} ${bottomFontWeight} ${bottomFontSize}px ${bottomFontFamily}`
     ctx.fillStyle = color
-    ctx.textAlign = 'center'
+    ctx.textAlign = bottomFontPosition
+  
 
     bottomText.split('\n').reverse().forEach(function (t, i) {
       ctx.fillText(t, image.width / 2, image.height - i * bottomFontSize - 10, image.width);
@@ -47,9 +70,9 @@ const Canvas = ({ memes }) => {
 
   useEffect(() => {
     const topFontctx = canvas.current.getContext('2d')
-    topFontctx.font = `${topFontSize}px Comic Sans MS`
+    topFontctx.font = `${topFontStyle} ${topFontWeight} ${topFontSize}px ${topFontFamily}`
     topFontctx.fillStyle = color
-    topFontctx.textAlign = 'center'
+    topFontctx.textAlign = topFontPosition
 
     topText.split('\n').forEach(function (t, i) {
       topFontctx.fillText(t, image.width / 2,40 + i * topFontSize, image.width);
@@ -85,6 +108,28 @@ const Canvas = ({ memes }) => {
         <div className="editor__text-control-panel">
           <input type="color" onChange={e => fillStyle(e)} className="editor__color" />
           <input type="range" onChange={e => changeFontSize(e.target.value, setTopFontSize)} min="10" max="72" defaultValue="50" step="1"/>
+          <button onClick={e => increaseSize(topFontSize, setTopFontSize)} className="icon-size">A+</button>
+          <button onClick={e => decreaseSize(topFontSize, setTopFontSize)} className="icon-size">A-</button>
+          <button onClick={e => setTopFontPosition('end')}>
+            <img src={textLeft} alt="Текст слева" className="icon" />
+          </button>
+          <button onClick={e => setTopFontPosition('center')}>
+            <img src={textCenter} alt="Текст по середине" className="icon" />
+          </button>
+          <button onClick={e => setTopFontPosition('start')}>
+            <img src={textRight} alt="Текст справа" className="icon" />
+          </button>
+          <button onClick={e => setTopFontWeight('bold')}>
+            <img src={textBold} alt="Жирный текст" className="icon" />
+          </button>
+          <button onClick={e => setTopFontStyle('italic')}>
+            <img src={textItalic} alt="Курсивный текст" className="icon" />
+          </button>
+          <select>
+            <option onClick={e => setTopFontFamily('Comic Sans MS')}>Comic Sans MS</option>
+            <option onClick={e => setTopFontFamily('Arial')}>Arial</option>
+            <option onClick={e => setTopFontFamily('Serif')}>Serif</option>
+          </select>
         </div>
         <textarea
           className="editor__text"
@@ -98,6 +143,28 @@ const Canvas = ({ memes }) => {
         <div className="editor__text-control-panel">
           <input type="color" onChange={e => fillStyle(e)} className="editor__color" />
           <input type="range" onChange={e => changeFontSize(e.target.value, setBottomFontSize)} min="10" max="72" defaultValue="50" step="1"/>
+          <button onClick={e => increaseSize(bottomFontSize, setBottomFontSize)} className="icon-size">A+</button>
+          <button onClick={e => decreaseSize(bottomFontSize, setBottomFontSize)} className="icon-size">A-</button>
+          <button onClick={e => setBottomFontPosition('end')}>
+            <img src={textLeft} alt="Текст слева" className="icon" />
+          </button>
+          <button onClick={e => setBottomFontPosition('center')}>
+            <img src={textCenter} alt="Текст по середине" className="icon" />
+          </button>
+          <button onClick={e => setBottomFontPosition('start')}>
+            <img src={textRight} alt="Текст справа" className="icon" />
+          </button>
+          <button onClick={e => setBottomFontWeight('bold')}>
+            <img src={textBold} alt="Жирный текст" className="icon" />
+          </button>
+          <button onClick={e => setBottomFontStyle('italic')}>
+            <img src={textItalic} alt="Курсивный текст" className="icon" />
+          </button>
+          <select>
+            <option onClick={e => setBottomFontFamily('Comic Sans MS')}>Comic Sans MS</option>
+            <option onClick={e => setBottomFontFamily('Arial')}>Arial</option>
+            <option onClick={e => setBottomFontFamily('Serif')}>Serif</option>
+          </select>
         </div>
         <textarea
           className="editor__text"
