3b377fd21df74bcb4104b072878fe74d60b6da33
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-02-11 13:06:24 +0400

    add acivity to button memesbox__bth-show-more, add z-index to Link

--- frontend/src/components/MemesBox/MemesBox.jsx
+++ frontend/src/components/MemesBox/MemesBox.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import "./MemesBox.css"
 import lines from "../../images/lines.png"
 import lines2 from "../../images/lines2.png"
@@ -7,16 +7,27 @@ import Meme from "../Meme/Meme"
 import { HashLink as Link } from "react-router-hash-link";
 
 const MemesBox = ({ memes }) => {
+  const [numberOfVisibleMems, setNumberOfVisibleMems] = useState(21);
+  
+  const addMemes = () => {
+    setNumberOfVisibleMems(numberOfVisibleMems + 21);
+  };
+
   return (
     <section className="memesbox" aria-label="Box of memes">
       <img className="memesbox__lines memesbox__lines_left" src={lines} alt="Линии." />
       <img className="memesbox__lines memesbox__lines_right" src={lines2} alt="Линии." />
       <ul className="memesbox__container">
-        {memes.map((elem, index) => {
-          return <Meme image={elem.image} key={elem.id} index={index} />
+        {memes
+          .slice(0, numberOfVisibleMems)
+          .map((elem, index) => {
+            return <Meme image={elem.image} key={elem.id} index={index} />
         })}
       </ul>
-      <button className="memesbox__btn-show-more btn">показать больше</button>
+      {memes.length > numberOfVisibleMems && (
+        <button onClick={addMemes} className="memesbox__btn-show-more btn">показать больше</button>
+      )
+      }
       <Link to='/generator-meme#main' className="memesbox__up">
         <img className="memesbox__up-arrow" src={arrowUp} alt="Стрелка вверх." />
         <p className="memesbox__up-text" >Наверх</p>
