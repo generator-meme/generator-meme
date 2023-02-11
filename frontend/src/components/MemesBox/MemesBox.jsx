30a29e85540a62a47ec1d3b1bad6047b9832d10e
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-02-11 12:07:54 +0400

    create component MemesBox, change some style in Main and MemesBox, add onClick to main__btn, add hashLink to scroll main-page

---
+++ frontend/src/components/MemesBox/MemesBox.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import "./MemesBox.css"
+import lines from "../../images/lines.png"
+import lines2 from "../../images/lines2.png"
+import arrowUp from "../../images/arrow-up.svg"
+import Meme from "../Meme/Meme"
+// import { Link } from "react-router-dom"
+import { HashLink as Link } from "react-router-hash-link";
+
+const MemesBox = ({ memes }) => {
+  return (
+    <section className="memesbox" aria-label="Box of memes">
+      <img className="memesbox__lines memesbox__lines_left" src={lines} alt="Линии." />
+      <img className="memesbox__lines memesbox__lines_right" src={lines2} alt="Линии." />
+      <ul className="memesbox__container">
+        {memes.map((elem, index) => {
+          return <Meme image={elem.image} key={elem.id} index={index} />
+        })}
+      </ul>
+      <button className="memesbox__btn-show-more btn">показать больше</button>
+      <Link to='/generator-meme#main' className="memesbox__up">
+        <img className="memesbox__up-arrow" src={arrowUp} alt="Стрелка вверх." />
+        <p className="memesbox__up-text" >Наверх</p>
+      </Link>
+  </section>
+  )
+}
+
+export default MemesBox
