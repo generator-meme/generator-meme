fa950428c2ee8654dd8f398b5dfa66548560aa79
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-02-11 17:58:31 +0400

    change memesbox_up style

--- frontend/src/components/MemesBox/MemesBox.jsx
+++ frontend/src/components/MemesBox/MemesBox.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
 import "./MemesBox.css"
 import lines from "../../images/lines.png"
 import lines2 from "../../images/lines2.png"
@@ -7,11 +7,24 @@ import Meme from "../Meme/Meme"
 import { HashLink as Link } from "react-router-hash-link";
 
 const MemesBox = ({ memes }) => {
+  const [scrollTop, setScrollTop] = useState(null);
   const [numberOfVisibleMems, setNumberOfVisibleMems] = useState(21);
   
   const addMemes = () => {
     setNumberOfVisibleMems(numberOfVisibleMems + 21);
   };
+
+  const handleScroll = (event) => {
+    setScrollTop(window.scrollY);
+  };
+
+  useEffect(()=> {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [])
 
   return (
     <>
@@ -30,7 +43,7 @@ const MemesBox = ({ memes }) => {
           <button onClick={addMemes} className="memesbox__btn-show-more btn">показать больше</button>
         )
         }
-        <Link to='/generator-meme#memes-start' className="memesbox__up">
+        <Link to='/generator-meme#memes-start' className={` ${(scrollTop > 700) ? "memesbox__up_visible" : ""} memesbox__up`}>
           <img className="memesbox__up-arrow" src={arrowUp} alt="Стрелка вверх." />
           <p className="memesbox__up-text" >Наверх</p>
         </Link>
