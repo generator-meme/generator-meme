d57755fd23b51b82ae1692c1cb6b78c0b8e91cbf
stepan <stepa5564@gmail.com>
2023-02-23 10:52:22 +0300

    добавлен просмотр картинок из админки

--- backend/memes/admin.py
+++ backend/memes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 from .models import Meme, Tag, Template
 
 
@@ -13,7 +13,12 @@ class MemeAdmin(admin.ModelAdmin):
 @admin.register(Template)
 class TemplateAdmin(admin.ModelAdmin):
     '''Админ-панель модели Meme с фильтрацией по тегам'''
-    list_display = ('id', 'image')
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="150" height="100" />'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
+    list_display = ('id', 'image_tag')
     list_filter = ('tag',)
 
 
