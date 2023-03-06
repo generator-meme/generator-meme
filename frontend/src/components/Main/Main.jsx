f9b3e1410526a0584754e0ee5f2b8412e958cdd3
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-03-06 13:19:56 +0400

    change some styles on main, canvas, savedMeme

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -17,15 +17,21 @@ const Main = ({ memes, setCurrentMeme, setIsNewMeme }) => {
       <section className="main" aria-label="Main part">
         <img className="main__cat" src={cat} alt="Кот." />
         <h1 className="main__title">Генератор мемов</h1>
-        <div className="main__text-box">
-          <h3 className="main__text-advice">
-            Выберите шаблон для создания мема или 
-          </h3>
-          <div className="main__btn-block">
-            <button onClick={onClick} className="main__btn btn">загрузите изображение</button>
-            <img className="main__btn_help" src={help} alt='Подсказка.' />
+          <p className="main__text-advice">
+            Выберите шаблон для создания мема или&nbsp;
+            <span className="main__future-input">загрузите свое изображение</span>
+          </p>
+          {/* 
+          <div className="main__text-box">
+            <h3 className="main__text-advice">
+              Выберите шаблон для создания мема или 
+            </h3>
+            <div className="main__btn-block">
+              <button onClick={onClick} className="main__btn btn">загрузите изображение</button>
+              <img className="main__btn_help" src={help} alt='Подсказка.' />
+            </div>
           </div>
-        </div>
+          */}
       </section>
       <MemesBox memes={memes} setCurrentMeme={setCurrentMeme} setIsNewMeme={setIsNewMeme} />
     </main>
