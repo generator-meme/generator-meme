5da0b9ff49547726a5bf89418384d68f3fe649ab
a-menshikov <a.menshikov1989@gmail.com>
2023-04-05 21:33:31 +0300

    пагинация наверху change_list

--- backend/generator_meme/settings.py
+++ backend/generator_meme/settings.py
@@ -50,10 +50,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'generator_meme.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
