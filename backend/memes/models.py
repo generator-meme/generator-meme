f3f1920947c7f6e893af56ecbad4871146713594
a-menshikov <a.menshikov1989@gmail.com>
2023-04-14 11:31:34 +0300

    ордеринг для модели тега

--- backend/memes/models.py
+++ backend/memes/models.py
@@ -22,6 +22,7 @@ class Tag(models.Model):
     class Meta:
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
+        ordering = ('name',)
 
     def __str__(self) -> str:
         return self.name
