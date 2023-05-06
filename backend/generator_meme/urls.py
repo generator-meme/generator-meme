f7ef271059d79f44bc26976774bfdd8983a4d4b4
Aleksandr140590 <alex2717@yandex.kz>
2023-05-06 20:41:32 +0600

    Update urls.py

--- backend/generator_meme/urls.py
+++ backend/generator_meme/urls.py
@@ -4,7 +4,6 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-
 schema_view = get_schema_view(
     openapi.Info(
         title="Meme API",
