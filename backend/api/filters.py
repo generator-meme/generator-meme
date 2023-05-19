b027e02cea2534efa99a3337ce15439db44b4c79
a-menshikov <a.menshikov1989@gmail.com>
2023-05-19 14:43:14 +0300

    Добавлен фильтрсет для шаблонов

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,34 @@
+from django_filters import rest_framework as filters
 from rest_framework.filters import SearchFilter
+
+from memes.models import Template
+
+
+class MultiValueCharFilter(filters.BaseCSVFilter, filters.CharFilter):
+    """Множественный фильтр для параметра
+    Срабатывает, если объект имеет совпадение по
+    всем значениям из запроса."""
+
+    def filter(self, qs, value):
+        values = value or []
+
+        for value in values:
+            qs = super(MultiValueCharFilter, self).filter(qs, value)
+
+        return qs
+
+
+class TemplateFilter(filters.FilterSet):
+    """Фильтр для вью шаблонов мемов."""
+    tag = MultiValueCharFilter(lookup_expr='exact')
+
+    class Meta:
+        model = Template
+        fields = [
+            'tag'
+        ]
 
 
 class TagSearchFilter(SearchFilter):
-    '''Поиск тегов по имени'''
+    """Поиск тегов по имени"""
     search_param = 'name'
