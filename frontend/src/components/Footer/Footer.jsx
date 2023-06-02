d7639033a0dbb7da3703bcf967a3e72f6be954bb
Alexandr Podogas <a.podogas@gmail.com>
2023-06-02 17:22:54 +0400

    add team page

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,21 @@
-import './Footer.css'
+import "./Footer.css";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return <div className="footer"></div>
-}
+  return (
+    <div className="footer">
+      <NavLink
+        end
+        to="/team"
+        className={({ isActive }) =>
+        `footer__link-team
+        ${isActive ? "footer__link-team--active": ""}`
+        }
+      >
+        Команда
+      </NavLink>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
