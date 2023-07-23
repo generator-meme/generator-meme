f14ceafc3679a4c89493534f7ee2a380bfd7fd6d
Aleksandr140590 <alex2717@yandex.kz>
2023-07-23 12:35:42 +0600

    Update settings.py

--- backend/generator_meme/settings.py
+++ backend/generator_meme/settings.py
@@ -192,6 +192,17 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/api/auth/social/token'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
 
 # записывать логи
 # документация https://pypi.org/project/drf-api-logger/
