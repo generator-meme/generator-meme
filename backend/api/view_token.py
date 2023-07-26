968d483bdfcb5682c733be3adc4afd66ef93de27
Aleksandr140590 <alex2717@yandex.kz>
2023-07-26 22:30:19 +0600

    Update view_token.py

--- backend/api/view_token.py
+++ backend/api/view_token.py
@@ -1,9 +1,7 @@
 from django.shortcuts import redirect
-from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 
 
 @api_view(['GET'])
