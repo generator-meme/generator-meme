4db209de6b661abacbab41f42263aff90e08a6ce
Aleksandr140590 <alex2717@yandex.kz>
2023-08-21 21:54:52 +0600

    tests_beginning

--- tests/test_sws.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.mark.django_db(transaction=True)
-class TestSumm:
-    def test_passing(self, anonymous_client):
-        response = anonymous_client.get('/api/auth/users/me/')
-        assert response.status_code != 200
