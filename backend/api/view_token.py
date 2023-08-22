02408df7c7d918b0d6e44d8d8efd73a33cecb332
Aleksandr140590 <alex2717@yandex.kz>
2023-08-22 23:42:10 +0600

    Update view_token.py

--- backend/api/view_token.py
+++ backend/api/view_token.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import logout
 from django.shortcuts import redirect
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
@@ -10,5 +11,6 @@ def set_token(request):
     user = request.user
     if user.is_authenticated:
         token, _ = Token.objects.get_or_create(user=user)
+        logout(request)
         return redirect(f'/auth/social/{token}')
     return redirect('/')
