5d30d45b14d97e39c15607aa350d4f280a003369
Aleksandr140590 <alex2717@yandex.kz>
2023-08-22 22:25:14 +0600

    Update settings.py

--- backend/generator_meme/settings.py
+++ backend/generator_meme/settings.py
@@ -137,8 +137,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
 
