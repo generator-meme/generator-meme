42c3c676fb553dc14459e3faeadc0d705015bcd3
Roman Nosov <85586036+RomanNV@users.noreply.github.com>
2024-03-20 20:14:12 +0300

    remove console.log

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -48,7 +48,6 @@ const App = () => {
       .createNewMem(memeUrl, memeId)
       .then((res) => {
         setNewMeme(res);
-        console.log(JSON.stringify(res));
         localStorage.setItem("createdMeme", JSON.stringify(res));
       })
       .catch((err) => {
